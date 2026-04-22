9b9c1602527fcffb3a4ebc8ba92543fb56522b30
shehandanuka2020-a11y <shehandanuka2020@gmail.com>
2026-04-22 17:11:31 +0530

    pid_settings.h

---
+++ pid_settings.h
@@ -0,0 +1,15 @@
+PID Tuning Parameters
+ * Optimized for stable cinematic flight and GPS hold.
+ */
+
+#define ROLL_KP  1.35
+#define ROLL_KI  0.05
+#define ROLL_KD  18.0
+
+#define PITCH_KP 1.40
+#define PITCH_KI  0.05
+#define PITCH_KD  20.0
+
+#define YAW_KP   2.0
+#define YAW_KI   0.0
+#define YAW_KD   0.0
